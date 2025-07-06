c2059f59ce63441e5bb12ca1c95e304ecd35f0fd
emp88web <empire88b@gmail.com>
2025-07-06 19:40:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Empire88: Situs Gaming Resmi Mudah Menang Hari Ini")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Empire88 hadir sebagai situs gaming online resmi yang mudah menang hari ini. Coba menangkan hadiah besar jutaan rupiah besar bersama Empire88 sekarang juga!"
 )
 st.link_button("Daftar Disini", "https://cutt.ly/Zeyl9GaN")
 st.link_button("Login", "https://cutt.ly/Zeyl9GaN")
