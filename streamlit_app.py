f3cb8570bb1443ea4fb62e429e165cbb8d03e0c7
emp88web <empire88b@gmail.com>
2025-07-06 19:39:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.button(""Klik Saya", head over to [Daftar Disini](https://cutt.ly/Zeyl9GaN).")
+st.link_button("Daftar Disini", "https://cutt.ly/Zeyl9GaN")
+st.link_button("Login", "https://cutt.ly/Zeyl9GaN")
